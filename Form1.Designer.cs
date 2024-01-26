81857a77a387569bb061d86107dd8f188260842c
strator admini <yamaa.com987654321@outlook.jp>
2024-01-26 17:32:01 +0900

    レイアウト

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            weather_pic_box = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)weather_pic_box).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(weather_pic_box);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1784, 172);
+            panel1.TabIndex = 0;
+            panel1.Paint += panel1_Paint;
+            panel1.Resize += panel1_Resize;
+            // 
+            // weather_pic_box
+            // 
+            weather_pic_box.Image = (Image)resources.GetObject("weather_pic_box.Image");
+            weather_pic_box.Location = new Point(1358, 12);
+            weather_pic_box.Name = "weather_pic_box";
+            weather_pic_box.Size = new Size(82, 82);
+            weather_pic_box.TabIndex = 0;
+            weather_pic_box.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1784, 861);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)weather_pic_box).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private PictureBox weather_pic_box;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,16 @@
+using System.Diagnostics;
+using System.Drawing.Drawing2D;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
 namespace study_scheduler
 {
     public partial class Form1 : Form
@@ -6,5 +19,49 @@ namespace study_scheduler
         {
             InitializeComponent();
         }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            // ブロック（{}）を抜けたら自動的に Dispose するC#構文
+            using (
+                // グラデーションブラシ作成
+                var gb = new LinearGradientBrush(
+                // グラデーション範囲（表示クリッピング領域）
+                e.Graphics.VisibleClipBounds,
+                // グラデーション開始色（紺色）
+                Color.Lime,
+
+                // グラデーション終了色（赤紫）
+                Color.Cyan,
+                // グラデーション方向（縦）
+                LinearGradientMode.Horizontal))
+            {
+                // 四角形の内部を塗りつぶす（表示クリッピング領域）
+                e.Graphics.FillRectangle(gb, e.Graphics.VisibleClipBounds);
+            }
+            // using構文を使用したため Dispose を書く必要はない
+            //リソースを解放する
+            //gb.Dispose();
+        }
+
+        private void panel1_Resize(object sender, EventArgs e)
+        {
+            // パネルの表面全体を無効化してパネルを再描画する
+            panel1.Invalidate();
+        }
+
+
+        private void pic()
+        {
+            Image img =Image.FromFile(@"resor");
+
+            weather_pic_box.Image = img;
+        }
+
+
+
+
+
+
     }
 }
